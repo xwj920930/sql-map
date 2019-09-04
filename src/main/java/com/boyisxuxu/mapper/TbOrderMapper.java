420ffb830140e21e55b1a2ffdfc26bfaac9c5158
xuwenjie <xuwenjie@travelsky.com>
2019-09-04 09:54:04 +0800

    init

---
+++ src/main/java/com/boyisxuxu/mapper/TbOrderMapper.java
@@ -0,0 +1,17 @@
+package com.boyisxuxu.mapper;
+
+import com.boyisxuxu.po.TbOrder;
+
+public interface TbOrderMapper {
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    TbOrder selectByPrimaryKey(String orderId);
+
+    int updateByPrimaryKeySelective(TbOrder record);
+
+    int updateByPrimaryKey(TbOrder record);
+}
---
+++ src/main/java/com/boyisxuxu/mapper/TbOrderShippingMapper.java
@@ -0,0 +1,17 @@
+package com.boyisxuxu.mapper;
+
+import com.boyisxuxu.po.TbOrderShipping;
+
+public interface TbOrderShippingMapper {
+    int deleteByPrimaryKey(String orderId);
+
+    int insert(TbOrderShipping record);
+
+    int insertSelective(TbOrderShipping record);
+
+    TbOrderShipping selectByPrimaryKey(String orderId);
+
+    int updateByPrimaryKeySelective(TbOrderShipping record);
+
+    int updateByPrimaryKey(TbOrderShipping record);
+}
